8a63bc1e4db691406e2c89dd6dba1c011c82c4a6
unknown <mohammedabdurahoof527@gmail.com>
2023-07-10 14:41:22 +0530

    mongodb-atlas added

--- confiq/connection.js
+++ confiq/connection.js
@@ -4,10 +4,10 @@ const state={
 }
 
 module.exports.connect=function(done){
-    const url='mongodb://localhost:27017'
+    const url='mongodb://mohammedabdurahoof527:aB-8943485194@ac-pmfimly-shard-00-00.paqexqg.mongodb.net:27017,ac-pmfimly-shard-00-01.paqexqg.mongodb.net:27017,ac-pmfimly-shard-00-02.paqexqg.mongodb.net:27017/?ssl=true&replicaSet=atlas-fux4aj-shard-0&authSource=admin&retryWrites=true&w=majority'
     const dbname='megazin'
 
-    mongoClient.connect(url,(err,data)=>{
+    mongoClient.connect(url,{ useUnifiedTopology: true },(err,data)=>{
         if(err) return done(err)
         state.db=data.db(dbname)
         done()
